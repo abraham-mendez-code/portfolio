8f55380bdf0d82db48e32c05ab3d3760a5214923
abraham-mendez-code <abraham.me001@gmail.com>
2025-11-03 15:21:16 -0500

    Updated FinanceApplication.main() to test deposit method

--- src/main/java/com/pluralsight/finance/FinanceApplication.java
+++ src/main/java/com/pluralsight/finance/FinanceApplication.java
@@ -4,13 +4,13 @@ public class FinanceApplication {
 
     public static void main(String[] args) {
 
-        /* Test accounts once all methods are fully defined
-        BankAccount account1 = new BankAccount(123, "Pam", 12500);
-        Valuable account2 = new BankAccount(456, "Gary", 1500);
+
+        BankAccount account1 = new BankAccount("Pam", "123", 12500);
+        Valuable account2 = new BankAccount("Gary", "456", 1500);
 
         account1.deposit(100);
-        account2.deposit(100);
-        */
+       // account2.deposit(100);
+
     }
 
 }
