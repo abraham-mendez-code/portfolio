59bcd335f5b79ba45725b04065e12bae6e825d36
abraham-mendez-code <abraham.me001@gmail.com>
2025-11-03 14:38:17 -0500

    Added the FinanceApplication class

---
+++ src/main/java/com/pluralsight/finance/FinanceApplication.java
@@ -0,0 +1,16 @@
+package com.pluralsight.finance;
+
+public class FinanceApplication {
+
+    public static void main(String[] args) {
+
+        /* Test accounts once all methods are fully defined
+        BankAccount account1 = new BankAccount(123, "Pam", 12500);
+        Valuable account2 = new BankAccount(456, "Gary", 1500);
+
+        account1.deposit(100);
+        account2.deposit(100);
+        */
+    }
+
+}
