696eb96e82677758f5b8cbd03a475cf23243fddb
abraham-mendez-code <abraham.me001@gmail.com>
2025-11-03 15:17:16 -0500

    Changed access modifier to protected for class attributes

--- src/main/java/com/pluralsight/finance/Asset.java
+++ src/main/java/com/pluralsight/finance/Asset.java
@@ -3,8 +3,8 @@ package com.pluralsight.finance;
 public abstract class Asset implements Valuable {
 
     // class attributes
-    private String name;
-    private double marketValue;
+    protected String name;
+    protected double marketValue;
 
     // constructor
     public Asset(String name, double marketValue) {
